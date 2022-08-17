df53896d044475b249e3c7161efd5af7d5b7b84e
Alina <80460626+alishney@users.noreply.github.com>
2022-08-17 18:10:22 +0300

    corrected errors and inaccuracies

--- Coffee Machine/index.js
+++ Coffee Machine/index.js
@@ -3,81 +3,107 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-let bank = {
+const bank = {
     water: 400,
     milk: 540,
     beans: 120,
     cups: 9,
-    money: 550
-};
-
-let units = ["ml", "g", "$"];
-
-function output() {
-    console.log(`The coffee machine has:
+    money: 550,
+    actionFill: function () {
+        let availableAmount = Number(input("Write how many ml of water you want to add:\n"));
+        bank.water += availableAmount;
+        availableAmount = Number(input("Write how many ml of milk you want to add:\n"));
+        bank.milk += availableAmount;
+        availableAmount = Number(input("Write how many grams of coffee beans you want to add:\n"));
+        bank.beans += availableAmount;
+        availableAmount = Number(input("Write how many disposable coffee cups you want to add:\n"));
+        bank.cups += availableAmount;
+    },
+    actionBuy: function () {
+        let action = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        let availableCoffee = true;
+        switch(action) {
+            case "1":
+                availableCoffee = (Math.min(Math.floor(bank.water / 250), Math.floor(bank.beans / 15), Math.floor(bank.cups)) > 0);
+                if(availableCoffee){
+                    console.log("I have enough resources, making you a coffee!");
+                    bank.water -= 250;
+                    bank.beans -= 16;
+                    bank.cups -= 1;
+                    bank.money += 4;
+                } else {
+                    console.log("Sorry, not enough water!");
+                }
+                break;
+            case "2":
+                availableCoffee = (Math.min(Math.floor(bank.water / 350), Math.floor(bank.milk / 75), Math.floor(bank.beans / 20), Math.floor(bank.cups)) > 0);
+                if(availableCoffee) {
+                    bank.water -= 350;
+                    bank.milk -= 75;
+                    bank.beans -= 20;
+                    bank.cups -= 1;
+                    bank.money += 7;
+                } else {
+                    console.log("Sorry, not enough water!");
+                }
+                break;
+            case "3":
+                availableCoffee = (Math.min(Math.floor(bank.water / 200), Math.floor(bank.milk / 100), Math.floor(bank.beans / 12), Math.floor(bank.cups)) > 0);
+                if(availableCoffee) {
+                    bank.water -= 200;
+                    bank.milk -= 100;
+                    bank.beans -= 12;
+                    bank.cups -= 1;
+                    bank.money += 6;
+                } else {
+                    console.log("Sorry, not enough water!");
+                }
+                break;
+            case "back":
+                break;
+        }
+    },
+    actionTake: function () {
+        console.log(`I gave you ${units[2]}${bank.money}`);
+        bank.money = 0;
+    },
+    printCoffeeMachineState: function () {
+        console.log(`The coffee machine has:
 ${bank.water} ${units[0]} of water
 ${bank.milk} ${units[0]} of milk
 ${bank.beans} ${units[1]} of coffee beans
 ${bank.cups} disposable cups
 ${units[2]}${bank.money} of money
 
-`);
+`);}
 };
 
-function buy(){
-    action = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-    switch(action) {
-        case "1":
-            bank.water -= 250;
-            bank.beans -= 16;
-            bank.cups -= 1;
-            bank.money += 4;
-            break;
-        case "2":
-            bank.water -= 350;
-            bank.milk -= 75;
-            bank.beans -= 20;
-            bank.cups -= 1;
-            bank.money += 7;
-            break;
-        case "3":
-            bank.water -= 200;
-            bank.milk -= 100;
-            bank.beans -= 12;
-            bank.cups -= 1;
-            bank.money += 6;
-            break;
-    }
-};
+Object.seal(bank);
 
-function fill() {
-    let amount = Number(input("Write how many ml of water you want to add:"));
-    bank.water += amount;
-    amount = Number(input("Write how many ml of milk you want to add:"));
-    bank.milk += amount;
-    amount = Number(input("Write how many grams of coffee beans you want to add:"));
-    bank.beans += amount;
-    amount = Number(input("Write how many disposable coffee cups you want to add:"));
-    bank.cups += amount;
-};
+const units = ["ml", "g", "$"];
+let i = true;
 
-function take() {
-    console.log(`I gave you ${units[2]}${bank.money}`);
-    bank.money = 0;
-};
-
-for(i = 0; i < 100; i++){
-    output();
-    let activity = input("Write action (buy, fill, take):");
-    switch(activity) {
+while(i){
+    const selectedAction  = input("Write action (buy, fill, take, remaining, exit):\n");
+    switch(selectedAction) {
         case "buy":
-            buy();
+            bank.actionBuy();
             break;
         case "fill":
-            fill();
+            bank.actionFill();
             break;
         case "take":
-            take();
+            bank.actionTake();
+            break;
+        case "remaining":
+            bank.printCoffeeMachineState();
+            break;
+        case "exit":
+            i = false;
             break;
     }
-};
+}
+
+
+
+
