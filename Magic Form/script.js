8c8571e78931e5e17a6494050883fdff437275d5
Alina <80460626+alishney@users.noreply.github.com>
2022-08-29 17:14:27 +0300

    magic form: added corrections

--- Magic Form/script.js
+++ Magic Form/script.js
@@ -1,8 +1,10 @@
-const ides = ['first-name', 'last-name', 'email', 'phone', 'company', 'address'];
+const inputElementsIdes  = ['first_name', 'last_name', 'email', 'phone', 'company', 'address'];
 
-const form = document.querySelector(".source-form")
+const form = document.querySelector(".source-form");
 
-function submissionAction() {
+const formValues = {};
+
+function submitFormToLocalStorage() {
     let alreadyAddedSubmissions = JSON.parse(
         localStorage.getItem('submissions')
     );
@@ -10,74 +12,47 @@ function submissionAction() {
     if(alreadyAddedSubmissions === null) {
         alreadyAddedSubmissions = [];
     }
-    alreadyAddedSubmissions.push({
-        firstname: document.getElementById('first-name').value,
-        lastname: document.getElementById('last-name').value,
-        email: document.getElementById('email').value,
-        phone: document.getElementById('phone').value,
-        company: document.getElementById('company').value,
-        address: document.getElementById('address').value
-    });
 
+    alreadyAddedSubmissions.push(formValues);
 
-        localStorage.setItem(
-            'submissions',
-            JSON.stringify(alreadyAddedSubmissions)
-        );
-
+    localStorage.setItem(
+        'submissions',
+        JSON.stringify(alreadyAddedSubmissions)
+    );
 }
 
 function updatingForm() {
-    let currentFromCondition = JSON.parse(
-        localStorage.getItem('currentFromCondition')
-    );
 
-    if(currentFromCondition === null) {
-        currentFromCondition = [];
-    }
-    currentFromCondition.push({
-        firstname: document.getElementById('first-name').value,
-        lastname: document.getElementById('last-name').value,
-        email: document.getElementById('email').value,
-        phone: document.getElementById('phone').value,
-        company: document.getElementById('company').value,
-        address: document.getElementById('address').value
-    });
+    let currentFormCondition = {};
 
+    inputElementsIdes.forEach(formInputFieldId => {
+        formValues[formInputFieldId] = document.getElementById(formInputFieldId).value;
+        currentFormCondition[formInputFieldId] = document.getElementById(formInputFieldId).value;
+    })
 
     localStorage.setItem(
-        'currentFromCondition',
-        JSON.stringify(currentFromCondition)
+        'currentFormCondition',
+        JSON.stringify(currentFormCondition)
     );
+
 }
 
-ides.forEach((id) => {
-    document.getElementById(id).value = localStorage.getItem(id)
-    document.getElementById(id).addEventListener('input', () => {
-            localStorage.setItem(id, String(document.getElementById(id).value))
-        }
-    )
+inputElementsIdes.forEach((id) => {
+    document.getElementById(id).addEventListener('input', updatingForm)
 })
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    submissionAction();
+    submitFormToLocalStorage();
     form.reset();
-
-    ides.forEach((id) => {
-        localStorage.setItem(id, '')
-    })
 })
 
-window.onstorage = (event) => {
-    if(event.key == 'submissions') {
-        return;
-    } else {
-        ides.forEach((id) => {
-                document.getElementById(id).value = localStorage.getItem(id)
+window.addEventListener('storage', (event) => {
+    if(event.key == 'currentFormCondition') {
+        inputElementsIdes.forEach((id) => {
+            const currentFormState = JSON.parse(localStorage.getItem('currentFormCondition'));
+            document.getElementById(id).value = currentFormState[id];
         })
     }
-
-}
+});
 
