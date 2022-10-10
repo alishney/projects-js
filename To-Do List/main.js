b870441b6473ece701b1ac1de5102be2e8b14913
Alina <80460626+alishney@users.noreply.github.com>
2022-10-10 19:44:55 +0300

    to do list: added media

--- To-Do List/main.js
+++ To-Do List/main.js
@@ -1,33 +1,32 @@
+"use strict";
 const listWrapper = document.querySelector('.list-wrapper');
 const addBtn = document.getElementById('add-task-button');
+const taskInput = document.getElementById('input-task');
 
 const actionBtnFunction = (event) => {
     const target = event.target;
-    if (!((target == addBtn) || (target.className == 'delete-btn'))) return;
 
-    if (target == addBtn) {
-        const newTask = document.getElementById('input-task');
-        const htmlTask = `<label class="task">
+    if (!(target === addBtn || target.className === 'delete-btn')) return;
+    if (target.className === 'delete-btn') return target.closest('.task').remove();
+    if (target === addBtn) {
+        const taskHtmlTemplate = `<label class="task">
                       <input type="checkbox" class="checkmark">
                       <span class="fake-checkmark"></span>
-                      <span class="task-text">${newTask.value}</span>
+                      <span class="task-text">${taskInput.value}</span>
                       <button class="delete-btn">x</button>
                   </label>`;
 
-        if (newTask.value.trim() !== '') {
-            document.getElementById('task-list').innerHTML += htmlTask;
-            newTask.value = ''
+        if (taskInput.value.trim() !== '') {
+            document.getElementById('task-list').innerHTML += taskHtmlTemplate;
+            taskInput.value = ''
         }
-        return;
     }
-
-    if (target.className == 'delete-btn') return target.closest('.task').remove();
 }
 
 listWrapper.addEventListener('click', (event) => {
     actionBtnFunction(event)
 })
 
-listWrapper.addEventListener('keypress', (event) => {
-    if(event.key = 'Enter') return actionBtnFunction(event);
+document.getElementById('input-task').addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') return actionBtnFunction(event);
 })
